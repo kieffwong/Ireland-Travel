974fa932b53f65912abdfa4b6da51920e40c06a1
Kieff <44449801+kieffwong@users.noreply.github.com>
2019-10-23 01:34:48 +0100

    Add files via upload

---
+++ nuxt.config.js
@@ -0,0 +1,92 @@
+
+let colors = require('./assets/colors.json')
+
+let config = {
+  title: 'Private Tour Ireland',
+  description: 'Tailor made tour in Ireland.',
+  url: 'https://.com',
+  image: '/share.jpg',
+  keywords: 'Travel, Holiday, Ireland, Dublin, Cliff of Moher, Giant Causeway, Glendalough, Phoenix Park, Guisness Store'
+}
+
+let routes = []
+routes.push('/project/hawaii')
+routes.push('/project/pacificheightscontemporary')
+routes.push('/project/brown')
+routes.push('/project/woodside')
+routes.push('/project/sho17')
+routes.push('/project/jacksonsquare')
+routes.push('/project/pacificheightstownhouse')
+routes.push('/project/sho13')
+routes.push('/project/cowhollow')
+routes.push('/project/Dublin')
+
+module.exports = {
+  /*
+  ** Headers
+  */
+  head: {
+    title: 'Private Tour Ireland',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: config.description },
+      { name: 'theme-color', content: colors.bunker },
+
+      // Schema.org
+      { hid: 'itemprop:name', itemprop: 'name', content: config.title },
+      { hid: 'itemprop:description', itemprop: 'description', content: config.description },
+      { hid: 'itemprop:image', itemprop: 'image', content: config.url + config.image },
+
+      // facebook
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: config.url },
+      { hid: 'og:image', property: 'og:image', content: config.url + config.image },
+
+      { hid: 'og:title', property: 'og:title', content: config.title },
+      { hid: 'og:description', property: 'og:description', content: config.description },
+
+      // twitter
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:image', content: config.url + config.image },
+
+      { hid: 'twitter:title', name: 'twitter:title', content: config.title },
+      { hid: 'twitter:description', name: 'twitter:description', content: config.description },
+
+    ],
+    link: [
+      { rel: 'manifest', href: '/manifest.json' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: colors.bunker },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    ]
+  },
+  /*
+  ** Progress-bar color
+  */
+  loading: { color: colors.bunker },
+  /*
+  ** Build configuration
+  */
+  css: [{ src: '~assets/stylus/main.styl', lang: 'stylus' }],
+  plugins: [
+    {src: '~plugins/ga.js', ssr: false},
+  ],
+  build: {
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  generate: { routes: routes }
+}
